e937cb3d3111c43753cc78832bacaa1226bd8fa1
Lloyd Philbrook <stuff@whatafool.com>
2011-12-07 18:03:36 +0800

    Intial plugin scafolding setup

---
+++ init.rb
@@ -0,0 +1,9 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_github do
+  name 'Redmine Github plugin'
+  author 'Lloyd Philbrook'
+  description 'This assists with Redmine GitHub integration.'
+  version '0.0.1'
+  url 'https://github.com/phoolish/redmine_github'
+end
